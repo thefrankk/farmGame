32e7172d9abbbe6895f48a20d4fcfa1b02205da4
gfleitas <gabriel.fleiitas@gmail.com>
2023-02-07 23:43:48 -0300

    Soy dios

--- Assets/_Scripts/DragAndDropSystem/IDragginItem.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDragginItem 
-{
-    public abstract void DoWork(GridObject gridObject);
-    public void SetItemInMouseManager();
-}
